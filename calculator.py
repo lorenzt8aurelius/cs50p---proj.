52f83407964f8042ad60b964f9a078e72c6f7fed
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-06 19:54:42 +0800

    Added some comments

--- calculator.py
+++ calculator.py
@@ -1,4 +1,11 @@
-students = int(input("Enter number of students: "))
-seats = int(input("Enter number of seats: "))
+#conversion or type casting
+
+
+#Implicit Conversion (Type Coercion)
+#Explicit Conversion (Type Casting)
+
+#means that makes the code more readable#
+students = float(input("Enter number of students: "))
+seats = float(input("Enter number of seats: "))
 
 print(students / seats)
