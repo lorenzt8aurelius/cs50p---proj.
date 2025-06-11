d7196e054441abbe8acd5f2e9f69f40bf585cbf5
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-11 13:44:45 +0800

    Added some comments

--- Hi.py
+++ Hi.py
@@ -1,3 +1,5 @@
+#def - defines a function called hello
+#to is a parameter that acts as a placeholder. In other words to wild hold whatever name the user types later.
 def hello(to):
     print("Hi there,", to)
 
