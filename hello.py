bae4cfd9f0bed8ca2ab59770f531760d033efee9
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-12 20:10:26 +0800

    Added another comment

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+# Remove whitespace from the str and capitalize the first letter of each word
 name = input("What's your name? ").strip().title()
 
 #f is a format string that is uses "{}" for it's variable
