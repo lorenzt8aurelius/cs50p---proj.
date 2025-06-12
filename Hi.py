7d29fda20a753f09d5849222ce107a9b2822bbc7
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-12 13:27:13 +0800

    Added another def function(main)

--- Hi.py
+++ Hi.py
@@ -1,7 +1,9 @@
 #def - defines a function called hello
 #to is a parameter that acts as a placeholder. In other words to wild hold whatever name the user types later.
+def main():
+    name = input("What's your name? ")
+    hello(name)
+
 def hello(to):
     print("Hi there,", to)
 
-name = input("What's your name?")
-hello(name)
