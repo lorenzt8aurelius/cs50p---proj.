166adf2cbdf5482c85ba3fda0adb72c4a79878f8
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-17 17:03:28 +0800

    changes in the concatenation

--- area.py
+++ area.py
@@ -1,11 +1,11 @@
 def area(lenght, width):
     return lenght * width
-    print(str(lenght * width))
+    print(str(lenght * width) + "square feet")
 
 def main():
     house = area(50, 20)
     yard = area(50, 50)
     total = house + yard
-    print(str(total) + "sqr feet")
+    print(str(total) + "total square feet")
 
 (main)
