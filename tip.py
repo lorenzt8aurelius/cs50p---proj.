77f552efe89621367ae5aabd2f7df274d91ad533
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-18 19:48:01 +0800

    defined some functions

--- tip.py
+++ tip.py
@@ -2,4 +2,10 @@ def main():
     dollars = dollars_float(input("How much was the meal? "))
     percent = percent_to_float(input("what percentage would you like to tip? "))
     tip = dollars * percent
+    print(f"leave ${tip:2f}")
+
+
+def dollars_to_float(d)
     
+
+def percent_to_float(d)
