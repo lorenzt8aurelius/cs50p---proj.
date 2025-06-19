ba6c2e2b426f955047c9199db9cc516560a0c416
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-19 19:53:38 +0800

    initial changes in the conditionals

--- grade.py
+++ grade.py
@@ -1,10 +1,10 @@
 score = int(input("Score: "))
 
-if score >= 90 and score <= 100:
+if 90 >= score <= 100:
     print("Grade: 'A'")
-elif score >= 80 and score < 90:
+elif 80 >= score < 90:
     print("Grade: 'B'")
-elif score >= 75 and score < 79:
+elif  75 >= score < 79:
     print("Grade: 'C'")
 else:
     print("Grade: D")
