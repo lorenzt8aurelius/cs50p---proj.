c0c7c48634752ec445bb3b180ee629b2a391bf8f
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-19 19:46:35 +0800

    added more conditionals

--- grade.py
+++ grade.py
@@ -4,3 +4,7 @@ if score >= 90 and score <= 100:
     print("Grade: 'A'")
 elif score >= 80 and score < 90:
     print("Grade: 'B'")
+elif score >= 75 and score < 79:
+    print("Grade: 'C'")
+else:
+    print("Grade: D")
