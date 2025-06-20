2a525e81d8bc3f736b1224f31cb769e6fe31878b
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-20 14:14:46 +0800

    fix the variable name

--- house.py
+++ house.py
@@ -1,6 +1,6 @@
 name = input("What's your name? ")
 
-if name == "Loreto" or name == "Marisa" or namen == "Giveny" or name ==  "Lorenz" or name == "Lorenmark":
+if name == "Loreto" or name == "Marisa" or name == "Giveny" or name ==  "Lorenz" or name == "Lorenmark":
     print("Candidier")
 else:
     print("Who? ")
