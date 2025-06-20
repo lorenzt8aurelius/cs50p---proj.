049daef6f32cb4f16ec75a5042c2628e1785982e
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-20 14:13:45 +0800

    Initial commit

---
+++ house.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+
+if name == "Loreto" or name == "Marisa" or namen == "Giveny" or name ==  "Lorenz" or name == "Lorenmark":
+    print("Candidier")
+else:
+    print("Who? ")
