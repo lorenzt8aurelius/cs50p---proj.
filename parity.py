564014fe495e7562d1855e9eeda32a4c421d462a
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-20 13:54:23 +0800

    Initial changes

--- parity.py
+++ parity.py
@@ -1,6 +1,6 @@
-x = int(input("What's x? "))
-
-if x % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+def main():
+    x = int(input("What is x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
