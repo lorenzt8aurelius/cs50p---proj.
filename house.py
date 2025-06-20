c2100b958876cb5351e4465a44b152bf3bae5ab3
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-20 19:34:11 +0800

    added a match statement

--- house.py
+++ house.py
@@ -1,6 +1,3 @@
 name = input("What's your name? ")
 
-if name == "Loreto" or name == "Marisa" or name == "Giveny" or name ==  "Lorenz" or name == "Lorenmark":
-    print("Candidier")
-else:
-    print("Who? ")
+match name:
