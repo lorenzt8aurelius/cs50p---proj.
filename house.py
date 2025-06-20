3626a91bfdd5828483ea6dbddd87d8d808f461af
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-20 19:38:19 +0800

    added case statements

--- house.py
+++ house.py
@@ -1,3 +1,7 @@
 name = input("What's your name? ")
 
 match name:
+    case "Lorenz" | "Shanshiru" | "Jay" | "Shaynne":
+        print("cute")
+    case _:
+        print("Who? you ain't cute bruh")
