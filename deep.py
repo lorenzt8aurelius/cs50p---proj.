3584d4fecf21b1c7a3b0809ab3da0dbd72cc05ec
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-25 19:43:08 +0800

    changes in the variables

--- deep.py
+++ deep.py
@@ -1,7 +1,7 @@
 def deep():
     text = input("What's the Answer to the Great Question of Life, the Universe and Everything? ") 
 
-    answer = answer.strip().lower()
+    text = answer.strip().lower()
 
     if text == "42" | "Forty two" | "Forty-two" :
         print("yes")
