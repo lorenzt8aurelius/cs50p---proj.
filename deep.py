c44f074c726fb027bd0b128afc982338fcb280b7
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-25 19:38:53 +0800

    added some conditionals

--- deep.py
+++ deep.py
@@ -1,2 +1,14 @@
 def deep():
-    text = input("What's the Answer to the Great Question of Life, the Universe and Everything? ") answer.strip().lower()
+    text = input("What's the Answer to the Great Question of Life, the Universe and Everything? ") 
+
+    answer = answer.strip().lower()
+
+    if text == "42" | "Forty two" | "Forty-two" | "forty two" | "forty-two":
+        print("yes")
+    else:
+        print("No")
+
+deep()
+        
+        
+
