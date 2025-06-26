9728c25c0f5c69876a6f77769c65385b14aa43ee
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-26 19:55:14 +0800

    updates in conditionals

--- extensions.py
+++ extensions.py
@@ -4,11 +4,17 @@ def main():
     file = file.strip().lower()
 
     if file.endswith (".gif"):
-        print("")
+        print("image/gif")
     elif file.endswith (".jpg") or file.endswith(".jpeg"):
-        print("")
+        print("image/jpeg")
     elif file.endswith (".png"):
-        print("")
+        print("image/png")
+    elif file.endswith (".pdf"):
+        print("application/pdf")
+    elif file.endswith (".txt"):
+        print("text/plain")
+    elif file.endswith (".zip"):
+        print("application/zip")
     else:
         print("application/octet-stream")
 
