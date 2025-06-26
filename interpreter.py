87de8701c122435961c7200717861f75fba7f6f6
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-26 20:11:49 +0800

    updated intepreter

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,19 @@
 def main():
-    express = input("Expressions: ")
+    expression = input("Expression: ")
 
-    x, op, z = expressions.split()
+    x, op, z = expression.split()
     x = float(x)
-    y = float(y)
+    z = float(z)
+
+    if op == "+":
+        result = x + z
+    elif op == "-":
+        result = x - z
+    elif op == "*":
+        result = x * z
+    elif op == "/":
+        result = x / z
+
+    print(f"{result:.1f}")
+
+main()
