48166a4f0f51f9d5658b702f9965d47690ad5181
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-26 16:15:35 +0800

    added some conditionals

--- bank.py
+++ bank.py
@@ -2,3 +2,12 @@ def main():
     greeting = input("Greeting: ")
 
     greeting = greeting.strip().lower()
+
+    if greeting.startwith("hello"):
+        print("0$") 
+    elif greeting.startwith("h"):
+        print("20$") 
+    else:
+        print("0$") 
+
+main()
