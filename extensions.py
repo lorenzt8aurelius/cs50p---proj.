07fe0a00edf5a7996d62dbdd66087e4fa4470706
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-26 19:51:17 +0800

    added more conditionals

--- extensions.py
+++ extensions.py
@@ -3,5 +3,14 @@ def main():
 
     file = file.strip().lower()
 
-    if file.endswith ("cat.gif"):
+    if file.endswith (".gif"):
         print("")
+    elif file.endswith (".jpg") or file.endswith(".jpeg"):
+        print("")
+    elif file.endswith (".png"):
+        print("")
+    else:
+        print("application/octet-stream")
+
+        
+main()
