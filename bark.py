d5bac04f58969d0b1556cda9b98796afcbef0ba6
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-27 13:20:21 +0800

    added a value

--- bark.py
+++ bark.py
@@ -5,6 +5,8 @@ def main():
 def get_number():
     while True:
         n = int(input("Enter the barks you want: "))
+            if n > 0:
+                return n
 
 def meow(n):
     for _ in range(n):
