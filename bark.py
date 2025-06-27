fd68e1c219cc3ebc27fd0428bb31978f6465e0ca
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-06-27 13:22:35 +0800

    initial updates

--- bark.py
+++ bark.py
@@ -1,12 +1,13 @@
 def main():
     number = get_number()
-    meow()
+    meow(number)
 
 def get_number():
     while True:
         n = int(input("Enter the barks you want: "))
-            if n > 0:
-                return n
+        if n > 0:
+            break
+    return n
 
 def meow(n):
     for _ in range(n):
