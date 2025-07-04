d38b70d92c732bc2a05c396215facd07858ee8f0
Lorenz Jay <lorenzt8aurelius@gmail.com>
2025-07-04 22:13:49 +0800

    fix some bugs

--- hogwarts.py
+++ hogwarts.py
@@ -1,7 +1,7 @@
 students = [
-    {"name:" "lorenz", "house": "Aleosan". "age": "19"}
-    {"name:" "Shanshiru", "house": "Alada". "age": "19"}
+    {"name": "lorenz", "house": "Aleosan", "age": "19"},
+    {"name": "Shanshiru", "house": "Alada", "age": "19"}
 ]
 
-for students in student:
-    print(student["name"], student["house"], student["age"] sep=",")
+for student in students:
+    print(student["name"], student["house"], student["age"], sep=", ")
